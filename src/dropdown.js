0ca84d904d134a2466f453504e1cb5a31b9f30f3
Stephen Sanders <scsandman6496@gmail.com>
2017-12-31 01:31:12 -0500

    edited a word

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,2 +1,2 @@
 $('.dropdown-toggle').dropdown(); // for main menu dropdown
-$('.collapse').collapse(); // for accordian on creative.html
+$('.collapse').collapse(); // for accordion on creative.html
