68173a4d29a00bea8697fca8f89e9d87f97b171a
Stephen C. Sanders <scsandman6496@gmail.com>
2018-12-06 21:48:31 -0500

    multiple edits, including secondary navbar styling, adding another poem to creative page, removing academic subsite, and a few minor tweaks

---
+++ src/creative.js
@@ -0,0 +1,11 @@
+if($(window).width() > 1316) {
+    $(function() {
+        // creative page mini nav
+        $('#creative-tabs').addClass('nav-stacked');
+        $('#creative-tabs').css("float", "left");
+    });
+}
+
+// to center the date in each card on creative page
+$('.card-body').css("width", "100%");
+$('.card-body div > p').css({"text-align": "center", "font-style": "italic"});
